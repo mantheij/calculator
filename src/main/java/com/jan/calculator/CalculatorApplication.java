9fe0a0d810d122cc3bea0a3297babc45270b0225
Jan <vitehd.de@gmail.com>
2024-08-22 18:05:43 +0200

    restructure

--- src/main/java/com/jan/calculator/CalculatorApplication.java
+++ src/main/java/com/jan/calculator/CalculatorApplication.java
@@ -17,33 +17,3 @@ public class CalculatorApplication {
 
 }
 
-@Component
-class MyCalculatorApplication implements CommandLineRunner {
-
-	Scanner scanner = new Scanner(System.in);
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Command Line Calculator\nPlease provide a number:");
-		int first = scanner.nextInt();
-		System.out.println("Please provide a second number:");
-		int second = scanner.nextInt();
-		System.out.println("What operation should be executed?\n(Available: +,-,*,/,sqrt)");
-		
-		String operator;
-		while(true){
-			operator  = scanner.next();
-			if (Arrays.asList("+", "-", "*", "/", "sqrt").contains(operator)) {
-				break;
-			}
-			System.out.println("Invalid operator, please choose from the list above.");
-		}
-		
-		//Calculate the results
-		Operations calculate = new Operations(first, second);
-		System.out.println("Calculated result:");
-		if(operator.equals("+")){
-			System.out.println(calculate.add());
-		}
-	}
-}
