e925ea07f0ecc9ceb494cb724f192b4ce415645d
jmanthei <vitehd.de@gmail.com>
2024-08-23 15:00:06 +0200

    all operators, text design / formatting

--- src/main/java/com/jan/calculator/MyCalculatorApplication.java
+++ src/main/java/com/jan/calculator/MyCalculatorApplication.java
@@ -2,8 +2,13 @@ package com.jan.calculator;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 @Component
 public class MyCalculatorApplication implements CommandLineRunner {
@@ -12,26 +17,62 @@ public class MyCalculatorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Command Line Calculator\nPlease provide a number:");
-        int first = scanner.nextInt();
-        System.out.println("Please provide a second number:");
-        int second = scanner.nextInt();
-        System.out.println("What operation should be executed?\n(Available: +,-,*,/,sqrt)");
 
+        double first = 0;
+        double second = 0;
         String operator;
+
+        final String RESET = "\u001B[0m";
+        final String RED = "\u001B[31m";
+        final String GREEN = "\u001B[32m";
+        final String ITALIC = "\u001B[3m";
+
+        //Check first number
+        System.out.println("Command Line Calculator\nPlease provide a number:");
+        while(true){
+            try{
+                first = scanner.nextDouble();
+                break;
+            }catch (InputMismatchException e){
+                System.out.println(RED + "X " + RESET + "Please enter a valid number " + ITALIC + "(use ',' as comma)");
+                scanner.next();
+            }
+        }
+
+        //Check second number
+        System.out.println("Please provide a second number:");
+        while(true){
+            try{
+                second = scanner.nextDouble();
+                break;
+            }catch (InputMismatchException e){
+                System.out.println(RED + "X " + RESET + "Please enter a valid number (use ',' as comma)");
+                scanner.next();
+            }
+        }
+
+        //Check the operator
+        System.out.println("What operation should be executed?\n(Available:" + GREEN + " + " + RESET + "," + GREEN + " - "
+                + RESET + "," + GREEN + " * " + RESET + "," + GREEN + " / " + RESET + "," + GREEN + " pwr" + RESET +")");
         while(true){
             operator  = scanner.next();
-            if (Arrays.asList("+", "-", "*", "/", "sqrt").contains(operator)) {
+            if (Arrays.asList("+", "-", "*", "/", "pwr").contains(operator)) {
                 break;
             }
-            System.out.println("Invalid operator, please choose from the list above.");
+            System.out.println(RED + "X " + RESET + "Invalid operator, please choose from the list above.");
         }
 
         //Calculate the results
         Operations calculate = new Operations(first, second);
-        System.out.println("Calculated result:");
-        if(operator.equals("+")){
-            System.out.println(calculate.add());
-        }
+
+        HashMap<String, BiFunction<Double, Double, Double>> operationsMap = new HashMap<>();
+        operationsMap.put("+", (a, b) -> calculate.add());
+        operationsMap.put("-", (a, b) -> calculate.subtract());
+        operationsMap.put("*", (a, b) -> calculate.multiply());
+        operationsMap.put("/", (a, b) -> calculate.divide());
+        operationsMap.put("pwr", (a, b) -> calculate.pwr());
+        double result = operationsMap.get(operator).apply(first, second);
+        System.out.println("Calculated result:\n" + RED + first + " " + operator + " " + second + " = " + result);
+
     }
 }
--- src/main/java/com/jan/calculator/Operations.java
+++ src/main/java/com/jan/calculator/Operations.java
@@ -2,15 +2,23 @@ package com.jan.calculator;
 
 public class Operations {
     
-    int first;
-    int second;
+    double first;
+    double second;
 
-    public Operations(int first, int second){
+    public Operations(double first, double second){
         this.first = first;
         this.second = second;
     }
 
-    public int add() {
+    public double add() {
         return first+second;
     }
+
+    public double subtract() { return first-second; }
+
+    public double multiply() { return first*second; }
+
+    public double divide() { return first/second; }
+
+    public double pwr() {return Math.pow(first, second); }
 }
